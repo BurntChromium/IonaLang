11cd2e3e3acf7185935b8cfcd5e9bacb568c2b10
Burnt Chrome <BurntChromium@outlook.com>
2024-10-09 19:03:24 -0400

    add alias parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,43 +1,67 @@
 //! Parse
 
 use chumsky::prelude::*;
-use std::sync::OnceLock;
 
 // -------------------- Shared Tools --------------------
 
 pub const RESERVED_KEYWORDS: [&str; 6] = ["import", "is", "derives", "struct", "fn", "enum"];
 
-static IDENT_PARSER: OnceLock<Box<dyn Parser<char, String, Error = Simple<char>> + Sync + Send>> =
-    OnceLock::new();
-
-fn get_ident_parser() -> &'static (dyn Parser<char, String, Error = Simple<char>> + Sync + Send) {
-    IDENT_PARSER.get_or_init(|| {
-        Box::new(
-            text::ident()
-                .try_map(|s: String, span| {
-                    if !RESERVED_KEYWORDS.contains(&&s.as_str()) {
-                        Ok(s)
-                    } else {
-                        Err(Simple::custom(span, "Unexpected keyword"))
-                    }
-                })
-                .padded(),
-        )
-    })
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Types {
+    Integer,
+    Float,
+    String,
+    List(Box<Types>),
+    Tuple(Vec<Types>),
+    Custom(String),
 }
 
-static CAMEL_CASE: OnceLock<Box<dyn Parser<char, String, Error = Simple<char>> + Sync + Send>> =
-    OnceLock::new();
+/// Parsing identifiers (stringy names that aren't reserved)
+fn ident_parser() -> impl Parser<char, String, Error = Simple<char>> {
+    text::ident()
+        .try_map(|s: String, span| {
+            if !RESERVED_KEYWORDS.contains(&&s.as_str()) {
+                Ok(s)
+            } else {
+                Err(Simple::custom(span, "Unexpected keyword"))
+            }
+        })
+        .padded()
+}
 
-fn get_camel_case_parser() -> &'static (dyn Parser<char, String, Error = Simple<char>> + Sync + Send)
-{
-    CAMEL_CASE.get_or_init(|| {
-        Box::new(
-            filter(|c: &char| c.is_ascii_uppercase())
-                .chain(filter(|c: &char| c.is_ascii_alphanumeric()).repeated())
-                .collect::<String>()
-                .padded(),
-        )
+fn camel_case_parser() -> impl Parser<char, String, Error = Simple<char>> {
+    filter(|c: &char| c.is_ascii_uppercase())
+        .chain(filter(|c: &char| c.is_ascii_alphanumeric()).repeated())
+        .collect::<String>()
+        .padded()
+}
+
+fn type_parser() -> impl Parser<char, Types, Error = Simple<char>> {
+    recursive(|type_parser| {
+        let ident = text::ident();
+
+        let basic_type = choice((
+            text::keyword("int").to(Types::Integer),
+            text::keyword("float").to(Types::Float),
+            text::keyword("str").to(Types::String),
+            ident.map(Types::Custom),
+        ));
+
+        let list_type = just("List")
+            .ignore_then(type_parser.clone().delimited_by(just('['), just(']')))
+            .map(|inner| Types::List(Box::new(inner)));
+
+        let tuple_type = just("Tuple")
+            .ignore_then(
+                type_parser
+                    .clone()
+                    .separated_by(just(','))
+                    .at_least(1)
+                    .delimited_by(just('['), just(']')),
+            )
+            .map(|inner| Types::Tuple(inner));
+
+        choice((list_type, tuple_type, basic_type))
     })
 }
 
@@ -46,16 +70,18 @@ fn get_camel_case_parser() -> &'static (dyn Parser<char, String, Error = Simple<
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum ASTNode {
     ImportStatement(Import),
+    TypeAliasDeclaration(TypeAlias),
     StructDeclaration(Object),
     EnumDeclaration(Object),
 }
 
 fn ast_parser() -> impl Parser<char, Vec<ASTNode>, Error = Simple<char>> {
     let import_node = import_parser().map(ASTNode::ImportStatement);
+    let alias_node = alias_parser().map(ASTNode::TypeAliasDeclaration);
     let struct_node = struct_parser().map(ASTNode::StructDeclaration);
     let enum_node = enum_parser().map(ASTNode::EnumDeclaration);
 
-    let node = import_node.or(struct_node).or(enum_node);
+    let node = import_node.or(alias_node).or(struct_node).or(enum_node);
 
     node.padded().repeated().then_ignore(end())
 }
@@ -73,9 +99,9 @@ pub struct Import {
 }
 
 fn import_parser() -> impl Parser<char, Import, Error = Simple<char>> {
-    let ident = get_ident_parser();
+    let ident = ident_parser();
 
-    let file_name = ident;
+    let file_name = ident_parser();
 
     let items = just("with")
         .ignore_then(ident.repeated().collect())
@@ -86,6 +112,31 @@ fn import_parser() -> impl Parser<char, Import, Error = Simple<char>> {
         .ignore_then(file_name.padded())
         .then(items)
         .map(|(file, items)| Import { file, items })
+        .then_ignore(just(';'))
+}
+
+// -------------------- Type Aliases --------------------
+
+/// Type aliases let you do stuff like `alias Salary = int`
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct TypeAlias {
+    name: String,
+    alias_of: Types,
+}
+
+fn alias_parser() -> impl Parser<char, TypeAlias, Error = Simple<char>> {
+    let camel_case = camel_case_parser();
+    let name = camel_case;
+    let alias_of = type_parser();
+
+    text::keyword("alias")
+        .ignore_then(name.padded())
+        .then(just('='))
+        .then(alias_of.padded())
+        .map(|(name, alias_of)| TypeAlias {
+            name: name.0,
+            alias_of,
+        })
         .then_ignore(just(';'))
 }
 
@@ -125,11 +176,11 @@ pub enum ObjectMethods {
 // -------------------- Struct Parsing --------------------
 
 pub fn struct_parser() -> impl Parser<char, Object, Error = Simple<char>> {
-    let ident = get_ident_parser();
+    let camel_case = camel_case_parser();
 
-    let camel_case = get_camel_case_parser();
-
-    let field = ident.then(ident).map(|(name, type_)| Field { name, type_ });
+    let field = ident_parser()
+        .then(ident_parser())
+        .map(|(name, type_)| Field { name, type_ });
 
     let fields = field.separated_by(just("::")).at_least(1);
 
@@ -146,7 +197,7 @@ pub fn struct_parser() -> impl Parser<char, Object, Error = Simple<char>> {
         text::keyword("Eq").to(ObjectMethods::Eq),
         text::keyword("Log").to(ObjectMethods::Log),
     ))
-    .or(ident.map(ObjectMethods::Custom));
+    .or(ident_parser().map(ObjectMethods::Custom));
 
     let properties = just("is")
         .ignore_then(property.padded().repeated())
@@ -176,14 +227,12 @@ pub fn struct_parser() -> impl Parser<char, Object, Error = Simple<char>> {
 // -------------------- Enum Parsing --------------------
 
 pub fn enum_parser() -> impl Parser<char, Object, Error = Simple<char>> {
-    let ident = get_ident_parser();
-
-    let camel_case = get_camel_case_parser();
+    let camel_case = camel_case_parser();
 
     // Types are optional in an enum field, so they get mapped to `<Empty>`
-    let field = ident
+    let field = ident_parser()
         .then(
-            ident
+            ident_parser()
                 .or_not()
                 .map(|opt_type| opt_type.unwrap_or_else(|| "<Empty>".to_string())),
         )
@@ -204,7 +253,7 @@ pub fn enum_parser() -> impl Parser<char, Object, Error = Simple<char>> {
         text::keyword("Eq").to(ObjectMethods::Eq),
         text::keyword("Log").to(ObjectMethods::Log),
     ))
-    .or(ident.map(ObjectMethods::Custom));
+    .or(ident_parser().map(ObjectMethods::Custom));
 
     let properties = just("is")
         .ignore_then(property.padded().repeated())
