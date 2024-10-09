596b7f1dff8d24be7b6c0ce9fb9d94a6a0b8ccfa
Burnt Chrome <BurntChromium@outlook.com>
2024-10-09 00:23:09 -0400

    can parse an ast of imports and structs

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,31 @@
-use chumsky::prelude::*;
-use ariadne::{Report, ReportKind, Source, Label, Color};
+mod parser;
 
-#[derive(Debug, PartialEq, Clone)]
-struct Field {
-    name: String,
-    type_: String,
-}
+use ariadne::{Color, Label, Report, ReportKind, Source};
 
-/// Properties for data types (Structs and Enums)
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum DataProperties {
-    Public,
-    Export,
-}
-
-/// `Derivable` methods for data types (Structs and Enums)
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum DataMethods {
-    Eq,
-    Log,
-    Custom(String),
-}
-
-#[derive(Debug, PartialEq, Clone)]
-struct Struct {
-    name: String,
-    fields: Vec<Field>,
-    props: Vec<DataProperties>,
-    derives: Vec<DataMethods>,
-}
-
-const RESERVED_KEYWORDS: [&str; 2] = ["is", "derives"];
-
-fn struct_parser() -> impl Parser<char, Struct, Error = Simple<char>> {
-    let ident = text::ident()
-        .try_map(|s: String, span| {
-            if !RESERVED_KEYWORDS.contains(&&s.as_str()) {
-                Ok(s)
-            } else {
-                Err(Simple::custom(span, "Unexpected keyword"))
-            }
-        })
-        .padded();
-
-    let camel_case = filter(|c: &char| c.is_ascii_uppercase())
-        .chain(filter(|c: &char| c.is_ascii_alphanumeric()).repeated())
-        .collect::<String>()
-        .padded();
-
-    let field = ident.then(ident).map(|(name, type_)| Field { name, type_ });
-
-    let fields = field.separated_by(just("::")).at_least(1);
-
-    let struct_property = choice((
-        text::keyword("Public").to(DataProperties::Public).labelled("Public"),
-        text::keyword("Export").to(DataProperties::Export).labelled("Export"),
-    ));
-
-    let struct_derives = choice((
-        text::keyword("Eq").to(DataMethods::Eq),
-        text::keyword("Log").to(DataMethods::Log),
-    ))
-    .or(ident.map(DataMethods::Custom));
-
-    let properties = just("is")
-        .ignore_then(struct_property.padded().repeated())
-        .or_not()
-        .map(|opt| opt.unwrap_or_default());
-
-    let derives = just("derives")
-        .ignore_then(struct_derives.padded().repeated())
-        .or_not()
-        .map(|opt| opt.unwrap_or_default());
-
-    just("struct")
-        .ignore_then(camel_case)
-        .then_ignore(just("="))
-        .then(fields)
-        .then(properties)
-        .then(derives)
-        .then_ignore(just(";"))
-        .map(|(((name, fields), properties), derives)| Struct {
-            name,
-            fields,
-            props: properties,
-            derives,
-        })
-        .labelled("struct definition")
-}
+use crate::parser::*;
 
 fn main() {
-    let input = "struct Employee = id int :: salary int is Public derives Log;";
+    let input = r#"
+        import math with sqrt pow;
+        struct Employee = id int :: salary int is Public derives Log;
+    "#;
+
     let file_id = "example.txt";
 
-    match struct_parser().parse(input) {
-        Ok(struct_def) => println!("{:?}", struct_def),
+    match parse_source(input) {
+        Ok(struct_def) => {
+            println!("successfully parsed file");
+            println!("{:#?}", struct_def);
+        }
         Err(e) => {
+            println!("{:#?}", e);
             let report = Report::build(ReportKind::Error, file_id, 0)
-                .with_message("Failed to parse struct definition")
-                .with_label(Label::new((file_id, 0..input.len()))
-                    .with_message("in this struct definition")
-                    .with_color(Color::Red))
+                .with_message("Failed to parse")
+                .with_label(
+                    Label::new((file_id, 0..input.len()))
+                        .with_message("in this line")
+                        .with_color(Color::Red),
+                )
                 .with_labels(e.into_iter().map(|e| {
                     let expected = if e.expected().next().is_some() {
                         e.expected()
@@ -111,11 +36,14 @@ fn main() {
                             .collect::<Vec<_>>()
                             .join(", ")
                     } else {
-                        "a valid identifier".to_string()  // Provide a fallback
+                        "a valid identifier".to_string() // Provide a fallback
                     };
-                    
-                    let found = e.found().map(|c| format!("'{}'", c)).unwrap_or_else(|| "<something else>".to_string());
-                    
+
+                    let found = e
+                        .found()
+                        .map(|c| format!("'{}'", c))
+                        .unwrap_or_else(|| "<something else>".to_string());
+
                     Label::new((file_id, e.span().start..e.span().end))
                         .with_message(format!("Expected {}, found {}", expected, found))
                         .with_color(Color::Yellow)
--- src/parser.rs
+++ src/parser.rs
@@ -1,148 +1,171 @@
-//! Parser Combinator 
+//! Parse
 
-use std::string::Drain;
+use chumsky::prelude::*;
+use std::sync::OnceLock;
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct SourceLocation {
+// -------------------- Shared Tools --------------------
+
+pub const RESERVED_KEYWORDS: [&str; 6] = ["import", "is", "derives", "struct", "fn", "enum"];
+
+static IDENT_PARSER: OnceLock<Box<dyn Parser<char, String, Error = Simple<char>> + Sync + Send>> =
+    OnceLock::new();
+
+fn get_ident_parser() -> &'static (dyn Parser<char, String, Error = Simple<char>> + Sync + Send) {
+    IDENT_PARSER.get_or_init(|| {
+        Box::new(
+            text::ident()
+                .try_map(|s: String, span| {
+                    if !RESERVED_KEYWORDS.contains(&&s.as_str()) {
+                        Ok(s)
+                    } else {
+                        Err(Simple::custom(span, "Unexpected keyword"))
+                    }
+                })
+                .padded(),
+        )
+    })
+}
+
+static CAMEL_CASE: OnceLock<Box<dyn Parser<char, String, Error = Simple<char>> + Sync + Send>> =
+    OnceLock::new();
+
+fn get_camel_case_parser() -> &'static (dyn Parser<char, String, Error = Simple<char>> + Sync + Send)
+{
+    CAMEL_CASE.get_or_init(|| {
+        Box::new(
+            filter(|c: &char| c.is_ascii_uppercase())
+                .chain(filter(|c: &char| c.is_ascii_alphanumeric()).repeated())
+                .collect::<String>()
+                .padded(),
+        )
+    })
+}
+
+// -------------------- AST --------------------
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum ASTNode {
+    ImportStatement(Import),
+    StructDeclaration(Struct),
+}
+
+fn ast_parser() -> impl Parser<char, Vec<ASTNode>, Error = Simple<char>> {
+    let import_node = import_parser().map(ASTNode::ImportStatement);
+    let struct_node = struct_parser().map(ASTNode::StructDeclaration);
+
+    let node = import_node.or(struct_node);
+
+    node.padded().repeated().then_ignore(end())
+}
+
+pub fn parse_source(source: &str) -> Result<Vec<ASTNode>, Vec<Simple<char>>> {
+    ast_parser().parse(source)
+}
+
+// -------------------- Imports --------------------
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Import {
     file: String,
-    line: usize,
-    offset: usize
+    items: Vec<String>,
 }
 
-impl SourceLocation {
-    fn new(filepath: &str) -> SourceLocation {
-        SourceLocation {
-            file: filepath.to_string(),
-            line: 0,
-            offset: 0
-        }
-    }
+fn import_parser() -> impl Parser<char, Import, Error = Simple<char>> {
+    let ident = get_ident_parser();
+
+    let file_name = ident;
+
+    let items = just("with")
+        .ignore_then(ident.repeated().collect())
+        .or_not()
+        .map(|opt_items| opt_items.unwrap_or_default());
+
+    just("import")
+        .ignore_then(file_name.padded())
+        .then(items)
+        .map(|(file, items)| Import { file, items })
+        .then_ignore(just(';'))
 }
 
-#[derive(Debug)]
-enum ParserErrorClass {
-    NotFound,
+// -------------------- Objects --------------------
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Field {
+    name: String,
+    type_: String,
 }
 
-#[derive(Debug)]
-pub struct ParserError {
-    loc: SourceLocation,
-    class: ParserErrorClass
+/// Properties for data types (Structs and Enums)
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ObjectProperties {
+    Public,
+    Export,
 }
 
-impl ParserError {
-    fn from_parser(parser: &Parser, class: ParserErrorClass) -> ParserError {
-        ParserError {
-            loc: parser.location.clone(),
-            class
-        }
-    }
+/// `Derivable` methods for data types (Structs and Enums)
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ObjectMethods {
+    Eq,
+    Log,
+    Custom(String),
 }
 
-/// Stateful parser for text
-/// 
-/// ### Fields
-/// 
-/// - `location`: where in the file are we?
-/// - `stream`: a stateful tracker for the input data
-/// - `errors`: any errors we encounter along the way
-/// - `commit`: state tracker - should the operation modify the state or do we roll back the operation? 
-#[derive(Debug)]
-pub struct Parser {
-    location: SourceLocation,
-    stream: String,
-    errors: Vec<ParserError>,
-    commit: bool
+// -------------------- Structs --------------------
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Struct {
+    name: String,
+    fields: Vec<Field>,
+    props: Vec<ObjectProperties>,
+    derives: Vec<ObjectMethods>,
 }
 
-impl Parser {
-    pub fn new(input: &str, filepath: &str) -> Parser {
-        Parser {
-            location: SourceLocation::new(filepath),
-            stream: input.to_string(),
-            errors: Vec::new(),
-            commit: true
-        }
-    }
+pub fn struct_parser() -> impl Parser<char, Struct, Error = Simple<char>> {
+    let ident = get_ident_parser();
 
-    /// Try to consume a pattern without returning
-    pub fn then(&mut self, pattern: &str) -> &mut Self {
-        let chunk_len = pattern.len();
-        if self.stream.starts_with(pattern) {
-            let drained = self.stream.drain(0..chunk_len).collect::<String>(); 
-            self.advance_loc(drained);
-            self
-        } else {
-            self.errors.push(ParserError::from_parser(self, ParserErrorClass::NotFound));
-            self
-        }
-    }
+    let camel_case = get_camel_case_parser();
 
-    pub fn capture(&mut self, pattern: fn(&str) -> (bool, usize)) -> Option<String> {
-        let (success, size) = pattern(&self.stream);
-        if success {
-            let drained = self.stream.drain(0..size).collect::<String>();
-            self.advance_loc(drained.clone());
-            return Some(drained);
-        } else {
-            return None;
-        }
-    }
+    let field = ident.then(ident).map(|(name, type_)| Field { name, type_ });
 
-    pub fn capture_as<T>(&mut self, pattern: fn(&str) -> (bool, usize, T)) -> Option<T> {
-        let (success, size, out) = pattern(&self.stream);
-        if success {
-            let drained = self.stream.drain(0..size).collect::<String>();
-            self.advance_loc(drained);
-            return Some(out);
-        } else {
-            return None;
-        }
-    }
+    let fields = field.separated_by(just("::")).at_least(1);
 
-    pub fn capture_many(&mut self, pattern: fn(&str) -> (bool, usize)) -> Vec<String> {
-        let mut output: Vec<String> = Vec::new();
-        let mut exhausted = false;
-        while !exhausted {
-            let result = self.capture(pattern);
-            match result {
-                Some(item) => output.push(item),
-                None => {exhausted = true}
-            }
-        }
-        output
-    }
+    let struct_property = choice((
+        text::keyword("Public")
+            .to(ObjectProperties::Public)
+            .labelled("Public"),
+        text::keyword("Export")
+            .to(ObjectProperties::Export)
+            .labelled("Export"),
+    ));
 
-    /// Consume without returning until a pattern is found
-    pub fn take_until(&mut self, pattern: &str) -> &mut Self {
-        loop {
-            if self.stream.starts_with(pattern) {
-                let drained = self.stream.drain(0..pattern.len()).collect::<String>(); 
-                self.advance_loc(drained);
-                return self;
-            } else {
-                if self.stream.len() > 1 {
-                    // Advance
-                    self.stream = self.stream[1..].to_string();    
-                } else {
-                    return self;
-                }
-            }
-        }
-    }
+    let struct_derives = choice((
+        text::keyword("Eq").to(ObjectMethods::Eq),
+        text::keyword("Log").to(ObjectMethods::Log),
+    ))
+    .or(ident.map(ObjectMethods::Custom));
 
-    /// Update the internal state tracking the location in the code
-    fn advance_loc(&mut self, chunk: String) {
-        if self.commit {
-            for char in chunk.chars() {
-                // 
-                if char == 0xA as char {
-                    self.location.line += 1;
-                    self.location.offset = 0;
-                } else {
-                    self.location.offset += 1;
-                }
-            }
-        } 
-    }
+    let properties = just("is")
+        .ignore_then(struct_property.padded().repeated())
+        .or_not()
+        .map(|opt| opt.unwrap_or_default());
+
+    let derives = just("derives")
+        .ignore_then(struct_derives.padded().repeated())
+        .or_not()
+        .map(|opt| opt.unwrap_or_default());
+
+    just("struct")
+        .ignore_then(camel_case)
+        .then_ignore(just("="))
+        .then(fields)
+        .then(properties)
+        .then(derives)
+        .then_ignore(just(";"))
+        .map(|(((name, fields), properties), derives)| Struct {
+            name,
+            fields,
+            props: properties,
+            derives,
+        })
+        .labelled("struct definition")
 }
