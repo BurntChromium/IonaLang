4bff5f632405a9fa2c647d9514f55b17fac363fb
Burnt Chrome <BurntChromium@outlook.com>
2024-10-23 23:23:30 -0400

    fixed whitespace bug in expression parsing

--- src/expression_parser.rs
+++ src/expression_parser.rs
@@ -108,33 +108,29 @@ impl Parser {
         }
 
         // Keep parsing infix expressions as long as they have higher precedence
-        while let Some(op_precedence) = self.peek_precedence() {
+        loop {
             self.skip_whitespace(); // Safe to skip here - we're looking for infix operators
 
-            if op_precedence < min_precedence {
+            if let Some(op_precedence) = self.peek_precedence() {
+                if op_precedence < min_precedence {
+                    break;
+                }
+                left = self.parse_infix(left.output.unwrap());
+                if left.output.is_none() {
+                    break;
+                }
+            } else {
                 break;
             }
-            left = self.parse_infix(left.output.unwrap());
         }
 
         left
     }
 
     fn parse_prefix(&mut self) -> ParserOutput<Expr> {
-        // We have to clone the peek because match is immutable and consume is mutable
+        // Don't skip whitespace here - we need to properly detect unary operators
+        // We have to clone to avoid mut+immutable issues
         match &self.peek().symbol.clone() {
-            Symbol::Integer(n) => {
-                self.consume();
-                ParserOutput::okay(Expr::IntegerLiteral(*n))
-            }
-            Symbol::Float(f) => {
-                self.consume();
-                ParserOutput::okay(Expr::FloatLiteral(*f))
-            }
-            // Symbol::String(s) => {
-            //     self.consume();
-            //     ParserOutput::okay(Expr::StringLiteral(s.clone()))
-            // }
             Symbol::Dash => {
                 self.consume();
                 self.skip_whitespace(); // Safe to skip after consuming the unary operator
@@ -148,24 +144,36 @@ impl Parser {
                     operand: Box::new(operand.output.unwrap()),
                 })
             }
+            Symbol::Integer(n) => {
+                self.consume();
+                ParserOutput::okay(Expr::IntegerLiteral(*n))
+            }
+            Symbol::Float(f) => {
+                self.consume();
+                ParserOutput::okay(Expr::FloatLiteral(*f))
+            }
             Symbol::ParenOpen => {
                 self.consume();
-                // Parse the expression inside the parentheses.
+                self.skip_whitespace(); // Safe to skip inside parentheses
                 self.parse_expr(0).and_then(|expr| {
-                    // Once the expression is successfully parsed, try to consume the closing parenthesis.
-                    self.then_ignore(Symbol::ParenClose).map(|_| expr) // If successful, return the parsed expression.
+                    self.skip_whitespace(); // Safe to skip before closing paren
+                    self.then_ignore(Symbol::ParenClose).map(|_| expr)
                 })
             }
             Symbol::Identifier(name) => {
                 self.consume();
-                // Look ahead to see if this is a function call
+                self.skip_whitespace(); // Safe to skip after identifier
+                                        // Look ahead to see if this is a function call
                 if self.peek().symbol == Symbol::ParenOpen {
                     self.parse_function_call(name.clone())
                 } else {
                     ParserOutput::okay(Expr::Variable(name.clone()))
                 }
             }
-            _ => self.single_error("Expected an expression"),
+            other => self.single_error(&format!(
+                "Expected the beginning of an expression, but found {:?}",
+                other
+            )),
         }
     }
 
@@ -197,7 +205,6 @@ impl Parser {
             | Symbol::And
             | Symbol::Or => {
                 let op_precedence = precedence(&self.peek().symbol);
-
                 let operator = self.parse_binary_operator();
                 if operator.output.is_none() {
                     return operator.transmute_error::<Expr>();
@@ -409,6 +416,31 @@ mod tests {
         let expected = Expr::FunctionCall {
             name: "add".to_string(),
             arguments: vec![Expr::IntegerLiteral(2), Expr::IntegerLiteral(5)],
+        };
+        assert_eq!(expected, out.output.unwrap());
+    }
+
+    #[test]
+    fn test_expr_7() {
+        let program_text = "add(2, 5 * a)";
+        // Lex
+        let mut lexer = Lexer::new("test");
+        lexer.lex(&program_text);
+        println!("{:#?}", lexer.token_stream);
+        // Parse
+        let mut parser = Parser::new(lexer.token_stream);
+        let out = parser.parse_expr(0);
+        println!("{:#?}", out);
+        let expected = Expr::FunctionCall {
+            name: "add".to_string(),
+            arguments: vec![
+                Expr::IntegerLiteral(2),
+                Expr::BinaryOp {
+                    left: Box::new(Expr::IntegerLiteral(5)),
+                    operator: BinaryOperator::Multiply,
+                    right: Box::new(Expr::Variable("a".to_string())),
+                },
+            ],
         };
         assert_eq!(expected, out.output.unwrap());
     }
